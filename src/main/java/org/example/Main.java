872dfb3f49ce8f4d26f3441c254ce6862ded1912
aysuabdullazade <abdullazadeaysu89@gmail.com>
2025-06-14 19:33:54 +0400

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+
+class Main {
+    public static void main(String[] args) {
+        byte number = 1;
+        short number2 = 2;
+        int number3 = 3;
+        long number4 = 4;
+        float number5 = 5;
+        double number6 = 6;
+        char number7 = 'a';
+        boolean number8 = true;
+        System.out.println(number++);
+        System.out.println(--number);
+        int a = 166;
+        int b = 288;
+        System.out.print(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(a = 5);
+        System.out.println(a += 3);
+        System.out.println(a -= 2);
+        System.out.println(a *= 2);
+        System.out.println(a /=2);
+        System.out.println(a %= 3);
+        System.out.println(a ==b);
+        System.out.println(a !=b);
+        System.out.println(a >= b);
+        System.out.println(a < b);
+        System.out.println(a >b);
+        System.out.println(a <= b);
+        System.out.println(a>= b);
+        System.out.println(a >0 && b>0);
+        System.out.println(! true);
+        System.out.println(a>0|| b>0);
+
+
+
+    }
+}
