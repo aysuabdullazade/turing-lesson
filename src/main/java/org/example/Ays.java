97c84fd76f110261bc4e876bdf77c8999bc4c4b3
aysuabdullazade <abdullazadeaysu89@gmail.com>
2025-06-16 16:54:54 +0400

    second commit

---
+++ src/main/java/org/example/Ays.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Ays {
+    public static void main(String[] args) {
+
+        int ay = 3; // yaz
+
+
+        System.out.println(((ay >= 3) && (ay <= 5)) ? "Yaz" :
+                (((ay >= 6) && (ay <= 8)) ? "Yay" : (((ay >= 9) && (ay <= 11)) ?
+                        "Payız" : (((ay == 12) || (ay == 1) || (ay == 2)) ?
+                        "Qış" : "Yanlış ay")))
+                );
+
+
+
+
+    }
+
+}
+
