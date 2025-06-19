6497fa584b3e3c0867acc021b24c75e1dd497240
aysuabdullazade <abdullazadeaysu89@gmail.com>
2025-06-19 17:33:13 +0400

    second commit

--- src/main/java/org/example/Ays.java
+++ src/main/java/org/example/Ays.java
@@ -5,19 +5,18 @@ import org.w3c.dom.ls.LSOutput;
 public class Ays {
     public static void main(String[] args) {
 
-        int ay = 3; // yaz
-
-
-        System.out.println(((ay >= 3) && (ay <= 5)) ? "Yaz" :
-                (((ay >= 6) && (ay <= 8)) ? "Yay" : (((ay >= 9) && (ay <= 11)) ?
-                        "Payız" : (((ay == 12) || (ay == 1) || (ay == 2)) ?
-                        "Qış" : "Yanlış ay")))
-                );
-
-
-
-
+////        int ay = 3; // yaz
+////
+////
+////        System.out.println(((ay >= 3) && (ay <= 5)) ? "Yaz" :
+////                (((ay >= 6) && (ay <= 8)) ? "Yay" : (((ay >= 9) && (ay <= 11)) ?
+////                        "Payız" : (((ay == 12) || (ay == 1) || (ay == 2)) ?
+////                        "Qış" : "Yanlış ay")))
+//        );
     }
 
 }
+
+
+
 
--- src/main/java/org/example/Day.java
+++ src/main/java/org/example/Day.java
@@ -2,6 +2,17 @@ package org.example;
 
 public class Day {
     public static void main(String[] args) {
+        int a = 80;
+        if (a < 40) {
+            System.out.println("Failed- One or more exam scores are too low");
+        } else if (a >= 85) {
+            System.out.println("Congratulations! You have earned  a Certifacate of Excellence");
 
+        } else if (a >= 60 && a >= 70 && a < 85) {
+            System.out.println("Well done!  You have earned a Certificate of Achievement");
+        } else if (a >= 50 && a < 70) {
+            System.out.println("You passed");
+        }
     }
 }
+
