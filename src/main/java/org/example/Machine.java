09396c0eca712fe6a8fe68f46811b57c44b765a3
aysuabdullazade <abdullazadeaysu89@gmail.com>
2025-06-19 18:51:55 +0400

    second commit

---
+++ src/main/java/org/example/Machine.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Machine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        switch (number) {
+            case 1:
+                System.out.println(" Chocolate  Bar -$2.00");
+            case 2:
+                System.out.println(" Soda -$1.50");
+            case 3:
+                System.out.println(" Chips -$1.20");
+            case 4:
+                System.out.println(" Sandwich -$3.00");
+            case 5:
+                System.out.println(" Water -$1.00");
+
+        }
+
+
+    }
+
+}
+
