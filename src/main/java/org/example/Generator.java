9f0b2b30c1555c9e67b19cb6e02f51b4de287961
aysuabdullazade <abdullazadeaysu89@gmail.com>
2025-06-20 16:33:48 +0400

    master

--- src/main/java/org/example/Generator.java
+++ src/main/java/org/example/Generator.java
@@ -3,10 +3,11 @@ package org.example;
 public class Generator {
     public static void main(String[] args) {
 
+        int a = 5;
+
         for (int i = 1; i <= 10; i++) {
-             { System.out.println( "1 x " + i +" = "  + i);}
 
-
-            }
+                System.out.println(i + "x" + a + " = " + (a * i));
         }
     }
+}
