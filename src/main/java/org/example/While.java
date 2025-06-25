8923b595741be37f6f34f1181815efd92c52eef8
aysuabdullazade <abdullazadeaysu89@gmail.com>
2025-06-25 18:06:27 +0400

    master

---
+++ src/main/java/org/example/While.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class While {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 20) {
+            if (i % 2 != 0) {
+
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
