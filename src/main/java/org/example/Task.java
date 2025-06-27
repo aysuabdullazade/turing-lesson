ae0487ec2274ffdc0e66679a1c6bb618c48805d9
aysuabdullazade <abdullazadeaysu89@gmail.com>
2025-06-28 02:16:46 +0400

    master

---
+++ src/main/java/org/example/Task.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) {
+
+//        Scanner scanner =new Scanner(System.in);
+//        System.out.println( "22 yaş ");
+//
+//        int yaş = scanner.nextInt();
+//        System.out.println( "22"+ "yaş");
+
+
+//        int a = 18;
+//        if (a > 18) {
+//            System.out.println(" sən böyüksən artıq");
+//        } else if (a < 18) {
+//            System.out.println("Uşaqsan");
+//        }else{
+//            System.out.println( " Artıq bir gəncsən");
+
+//        for(int i = 0; i < 10; i++) {
+//            {
+//                if (i == 3) {
+//
+//                    System.out.println("true");
+//                } else {
+//                    System.out.println("false");
+
+
+
+        StringBuffer sb = new StringBuffer( " Qarabağ ");
+        sb.append(" Azərbaycandır");
+        sb.append(" Dağlıq ");
+        sb.insert( 0, "Dağlıq");
+        sb.delete(30, sb.length());
+
+        System.out.println(sb);
+    }
+}
+
+
+
+
+
+
