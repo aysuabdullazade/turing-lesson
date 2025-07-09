f738f536cbe810e470b1b98b456add9f698f01c6
aysuabdullazade <abdullazadeaysu89@gmail.com>
2025-07-09 21:05:34 +0400

    master

---
+++ src/main/java/org/example/Moto.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Moto {
+    public String model;
+    public String color;
+    public String  engine;
+    public double price;
+
+    public Moto(String model, String color, String engine , double price) {
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+        this.price = price;
+
+    }
+        public String toString() {
+        return "Moto "+model+" "+color+" "+engine+" "+price;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/org/example/MotoAPP.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class MotoAPP {
+    public static void main(String[] args) {
+        Moto Aysu= new Moto("Yamaha","gray","4- stroke", 45.000);
+
+        System.out.println(Aysu);
+    }
+}
