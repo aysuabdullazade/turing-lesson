11e73de55597351a63d6126b390e242d84658dc8
aysuabdullazade <abdullazadeaysu89@gmail.com>
2025-07-10 16:37:22 +0400

    master

--- src/main/java/org/example/Moto.java
+++ src/main/java/org/example/Moto.java
@@ -3,30 +3,20 @@ package org.example;
 public class Moto {
     public String model;
     public String color;
-    public String  engine;
+    public String engine;
     public double price;
 
-    public Moto(String model, String color, String engine , double price) {
+    public Moto(String model, String color, String engine, double price) {
         this.model = model;
         this.color = color;
         this.engine = engine;
         this.price = price;
 
+
+
     }
-        public String toString() {
-        return "Moto "+model+" "+color+" "+engine+" "+price;
-        }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    public String toString() {
+        return "Moto " + model + " " + color + " " + engine + " " + price;
+    }
 }
--- src/main/java/org/example/MotoAPP.java
+++ src/main/java/org/example/MotoAPP.java
@@ -1,9 +1,15 @@
 package org.example;
 
 public class MotoAPP {
+
     public static void main(String[] args) {
+
         Moto Aysu= new Moto("Yamaha","gray","4- stroke", 45.000);
+        Moto Suzuki = new Moto("Suzuki","blue","4-stroke",50.000 );
+        Moto Ducati = new Moto("Ducati","gelb","V4", 27.000 );
 
         System.out.println(Aysu);
+        System.out.println(Suzuki);
+        System.out.println(Ducati);
     }
 }
