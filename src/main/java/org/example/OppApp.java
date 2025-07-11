ef3c136499c7b9f9f208b605c72f93b2ffa5546b
aysuabdullazade <abdullazadeaysu89@gmail.com>
2025-07-11 18:49:17 +0400

    master

--- src/main/java/org/example/OppApp.java
+++ src/main/java/org/example/OppApp.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class OppApp {
     public static void main(String[] args) {
-       Opp Name =new  Opp("Azra", 12.00,"Clothes",6);
+       Product Name =new Product("Azra", 12.00,"Clothes",6);
         System.out.println(Name);
 
 
--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,12 +1,12 @@
 package org.example;
 
-public class Opp {
+public class Product {
     public String name;
     public double price;
     public String catagory;
     public int stock;
 
-    public Opp(String name, double price, String catagory, int stock) {
+    public Product(String name, double price, String catagory, int stock) {
         this.name = name;
         this.price = price;
         this.catagory = catagory;
@@ -16,18 +16,19 @@ public class Opp {
     }
 
 
+
     public String toString() {
         return "Opp " + name + ", " + price + ", " + catagory + ", " + stock;
 
 
     }
 
-    public double calculatePrice(double percent) {
+    public double discountPrice(double percent) {
         double discount = price * percent / 100;
         return price - discount;
     }
 
-    public boolean isInstock(double price) {
+    public boolean isInStock(double price) {
         return stock > 0;
 
     }
